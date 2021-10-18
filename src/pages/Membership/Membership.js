9eb8c9e43fe339ef893719b263252e60f12f085b
zahangir710 <zahangir710@gmail.com>
2021-10-19 00:51:15 +0200

    first commit

---
+++ src/pages/Membership/Membership.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Membership = () => {
+  return (
+    <div>
+      <h1>This is Membership</h1>
+    </div>
+  );
+};
+
+export default Membership;
---
+++ src/pages/TrailTraining/TrailTraining.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrailTraining = () => {
+  return (
+    <div>
+      <h1>This is trail training</h1>
+    </div>
+  );
+};
+
+export default TrailTraining;
